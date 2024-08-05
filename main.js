aeb55ab7e805f739fe669c931179dbfce4c57806
SQCodes <paulsqmagadi@gmail.com>
2024-08-06 01:03:35 +0300

    modified:   index.html 	modified:   main.js

--- main.js
+++ main.js
@@ -37,12 +37,27 @@ navbarLinks.forEach(link => {
   link.addEventListener('click', function() {
     navbar.classList.toggle('active');
 
-    if (navbar.classList.contains('active')){
-      menuBtn.innerHTML = '<p><span class="material-symbols-outlined">close</span></p>';
-    }
-    else {
-      menuBtn.innerHTML = '<p><span class="material-symbols-outlined">menu</span></p>';
-    }
+    // Check if the 'active' class is present on the navbar
+  if (navbar.classList.contains('active')) {
+    // Change the inner HTML of the menu button to a close icon
+    menuBtn.innerHTML = '<p><span class="material-symbols-outlined">close</span></p>';
+    
+    // Set the border-bottom style of the .header
+    header.style.borderBottom = '0.5px solid var(--gray-color)';
+
+    // Set the border-radius style of the .header
+    header.style.borderRadius = '10px';
+
+  } else {
+    // Change the inner HTML of the menu button to a menu icon
+    menuBtn.innerHTML = '<p><span class="material-symbols-outlined">menu</span></p>';
+
+    // Set the border-bottom style of the .header to none
+    header.style.borderBottom = 'none';
+
+    // Set the border-radius style of the .header to none
+    header.style.borderRadius = 'none';
+  }
   });
 });
 
