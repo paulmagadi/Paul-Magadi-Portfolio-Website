8c128012fdf9506a1b7df21ea9a4274517334723
SQCodes <paulsqmagadi@gmail.com>
2024-08-05 22:01:12 +0300

    modified:   index.html 	modified:   main.css 	modified:   main.js

--- main.js
+++ main.js
@@ -7,3 +7,11 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+
+// btn = document.querySelector('.logo');
+// item = document.querySelector('.menu-button');
+
+// btn.addEventListener('click', function() {
+//   item.innerHTML = '<span class="material-symbols-outlined">close</span>'
+// })
